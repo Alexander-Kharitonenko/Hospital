482bed4195f7e93ac233bb7391b23867e7080c87
Alexander-Kharitonenko <73183653+Alexander-Kharitonenko@users.noreply.github.com>
2021-08-26 05:48:48 -0700

    delete db and access

--- src/Hospital.DataBase/Table/Patients.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Patients]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-	LastName NVARCHAR(MAX),
-	FirstName NVARCHAR(MAX),
-	Patronymic NVARCHAR(MAX),
-	Gender NVARCHAR(MAX),
-	ResidenceAddress NVARCHAR(MAX)
-)
